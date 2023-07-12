34dd19d3807544c419dbe4f61298b7828f5294ee
Paul Amani <67480521+pacis32@users.noreply.github.com>
2023-07-12 16:47:14 -0500

    name added

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express=require('express');
 const app= express();
 const port= 4000;
-
+const name='Paul Amani'
 app.get('/',(req,res)=>{
-    res.send('<h1> load ballancer Demo App</h1> <h4>Message:Success<h4> <p>Version: 1.0.0</p>')
+    res.send(`<h1> load ballancer Demo App  </h1> <h4>Message:Success<h4> <p>Version: 1.0.0</p> by ${name}`)
 });
 
 app.listen(port,()=>{
